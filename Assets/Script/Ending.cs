16cb38de395ee261e1e1d5d37f9db3a7c179a2aa
chisato <chisatokobo@gmail.com>
2022-03-07 13:51:34 +0900

    エンドロール作成

---
+++ Assets/Script/Ending.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+    Vector3 Staffrollposition;
+    public RectTransform rectTransform;
+    public float Endpos;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        Staffrollposition = rectTransform.anchoredPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (rectTransform.anchoredPosition.y < Endpos) {
+
+            Staffrollposition.y += 1f;
+            rectTransform.anchoredPosition = Staffrollposition;
+        } else {
+            Invoke("changeScene", 2.0f);
+        }
+    }
+
+    void changeScene() {
+        SceneManager.LoadScene("Title");
+    }
+}
